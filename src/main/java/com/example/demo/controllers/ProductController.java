ab13e0ee78ebfb91bda9d5580835ad260f7cea58
Rashmi Prabath <rashmi@codegen.net>
2020-02-20 11:37:40 +0530

    SRDP:JETHTHU: first-endpoint.

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (c) 2018 CodeGen International (pvt) Ltd.
+ * demo: ProductController
+ * Author: rashmi
+ * Date: 20. Feb. 2020
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductController
+{
+	@GetMapping
+	public String getName()
+	{
+		return "Jeththu";
+	}
+}
