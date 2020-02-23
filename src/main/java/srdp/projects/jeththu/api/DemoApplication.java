44fee0d9a2c2835c59550ee158e6670bf8728b99
rashmiprabhath <rashmiprabhath@gmail.com>
2020-02-23 11:42:07 +0530

    SRDP:JAPI: CORS configuration method level.

--- src/main/java/srdp/projects/jeththu/api/DemoApplication.java
+++ src/main/java/srdp/projects/jeththu/api/DemoApplication.java
@@ -2,14 +2,14 @@ package srdp.projects.jeththu.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan( basePackages = "srdp.projects.jeththu.api")
-public class DemoApplication {
+public class DemoApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main( String[] args )
+    {
+        SpringApplication.run( DemoApplication.class, args );
+    }
 
 }
--- src/main/java/srdp/projects/jeththu/api/controllers/ProductController.java
+++ src/main/java/srdp/projects/jeththu/api/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package srdp.projects.jeththu.api.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import srdp.projects.jeththu.api.models.ProductTest;
 
 /**
  * Copyright (c) 2018 CodeGen International (pvt) Ltd.
@@ -14,9 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController
 {
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping
-	public String getName()
+	public ProductTest getName()
 	{
-		return "Jeththu";
+		return new ProductTest( "Jeththu Products" );
 	}
 }
